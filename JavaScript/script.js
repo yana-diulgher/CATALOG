7c707c68e58e1fc7761d720fd62079dc92f39072
Yana Diulgher <142722623+yana-diulgher@users.noreply.github.com>
2026-09-11 15:55:20 +0300

    added an array of object data

---
+++ JavaScript/script.js
@@ -0,0 +1,110 @@
+const products = [
+  {
+    category: "men's_jacket",
+    code: "001",
+    name: "Nord Jacket",
+    quantity: 10,
+    size: ["XS", "S", "M", "L", "XL", "XXL", "XXXL"],
+    prices: {
+      Fashion: 950,
+      Tagaer: 980,
+      KOS: 970
+    }
+  },
+  {
+    category: "men's_jacket",
+    code: "002",
+    name: "Arctic Jacket",
+    quantity: 15,
+    size: ["XS", "S", "M", "L", "XL", "XXL", "XXXL"],
+    prices: {
+      Fashion: 900,
+      Tagaer: 930,
+      KOS: 920
+    }
+  },
+  {
+    category: "men's_jacket",
+    code: "003",
+    name: "Storm Jacket",
+    quantity: 13,
+    size: ["XS", "S", "M", "L", "XL", "XXL", "XXXL"],
+    prices: {
+      Fashion: 1500,
+      Tagaer: 1800,
+      KOS: 1700
+    }
+  },
+  { 
+    category: "women's_jacket",
+    code: "004",
+    name: "Lyra Jacket",
+    quantity: 19,
+    size: ["XS", "S", "M", "L", "XL"],
+    prices: {
+      Fashion: 1300,
+      Tagaer: 1450,
+      KOS: 1400
+    }
+  },
+  { 
+    category: "women's_jacket",
+    code: "005",
+    name: "Selena Jacket",
+    quantity: 20,
+    size: ["XS", "S", "M", "L", "XL"],
+    prices: {
+      Fashion: 1150,
+      Tagaer: 1250,
+      KOS: 1240
+    }
+  },
+  { 
+    category: "women's_jacket",
+    code: "006",
+    name: "Adele Jacket",
+    quantity: 5,
+    size: ["XS", "S", "M", "L", "XL"],
+    prices: {
+      Fashion: 980,
+      Tagaer: 1030,
+      KOS: 1000
+    }
+  },
+  {
+    category: "children's_jacket",
+    code: "007",
+    name: "Snowflake Jacket",
+    quantity: 13,
+    size: ["1 year", "1,5 years", "3 years", "4-5 years"],
+    prices: {
+      Fashion: 700,
+      Tagaer: 720,
+      KOS: 740
+    }
+  },
+  {
+    category: "children's_jacket",
+    code: "008",
+    name: "Pixie Jacket",
+    quantity: 20,
+    size: ["1 year", "1,5 years", "3 years", "4-5 years"],
+    prices: {
+      Fashion: 890,
+      Tagaer: 950,
+      KOS: 900
+    }
+  },
+  {
+    category: "children's_jacket",
+    code: "009",
+    name: "Teddy Jacket",
+    quantity: 8,
+    size: ["1 year", "1,5 years", "3 years", "4-5 years"],
+    prices: {
+      Fashion: 550,
+      Tagaer: 590,
+      KOS: 570
+    }
+  }
+];
